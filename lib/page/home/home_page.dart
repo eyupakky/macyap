652569fb97931b831724ed598452fbf2dd5088c4
eyupakky <eyup.akky@gmail.com>
2024-01-18 00:22:35 +0300

    commit

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -492,7 +492,7 @@ class _HomePageState extends State<HomePage> {
     } else {
       version = remoteConfig.getInt("android_version_number");
     }
-    if (version > 39) {
+    if (version > 40) {
       showDialog(
         barrierDismissible: false,
         context: context,
